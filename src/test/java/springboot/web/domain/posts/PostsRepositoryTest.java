433f1c6864a99cb2bced8c63db5563fb7bd29969
Lokie89 <traeuman@gmail.com>
2020-12-22 13:26:50 +0900

    Post update

---
+++ src/test/java/springboot/web/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,48 @@
+package springboot.web.domain.posts;
+
+import org.aspectj.lang.annotation.After;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import springboot.domain.posts.Posts;
+import springboot.domain.posts.PostsRepository;
+
+import java.util.List;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanUp() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("jojoldu@gmail.com")
+                .build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+
+        assertEquals(posts.getTitle(), title);
+        assertEquals(posts.getContent(), content);
+    }
+}
