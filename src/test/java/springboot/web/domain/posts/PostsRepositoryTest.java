6c01727674e843146a5ccd0a6235060f0e15cf30
Lokie89 <traeuman@gmail.com>
2020-12-22 13:46:36 +0900

    JPA

--- src/test/java/springboot/web/domain/posts/PostsRepositoryTest.java
+++ src/test/java/springboot/web/domain/posts/PostsRepositoryTest.java
@@ -1,8 +1,6 @@
 package springboot.web.domain.posts;
 
-import org.aspectj.lang.annotation.After;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
@@ -13,7 +11,10 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import springboot.domain.posts.Posts;
 import springboot.domain.posts.PostsRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -44,5 +45,21 @@ public class PostsRepositoryTest {
 
         assertEquals(posts.getTitle(), title);
         assertEquals(posts.getContent(), content);
+    }
+
+    @Test
+    public void BaseTimeEntity_등록() {
+        LocalDateTime now = LocalDateTime.of(2019, 6, 4, 0, 0, 0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+        List<Posts> postsList = postsRepository.findAll();
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>>>>>>>> createDate = " + posts.getCreatedDate() + " modifiedDate = " + posts.getModifiedDate());
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 }
